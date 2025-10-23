6cb32eaee4a0b2af7e655e192607d02aa9a8d1ae
vinayak <vinayakramavath1@gmail.com>
2025-10-23 19:28:31 +0530

    Updated code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # --------------------------
 # Configuration
-# --------------------------
+# ---------------------------
 st.set_page_config(
     page_title="Churn Prediction Dashboard",
     page_icon="📉",
